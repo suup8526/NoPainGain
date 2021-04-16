ba6b2d8733cf80d4b261a3bc4abc3d50557a4e43
Vasu Sharma <vasusharma@Vasus-MacBook-Pro.local>
2021-04-16 14:43:36 -0600

    data analyser layer which consumes messages sent by data_collector layer and exposes UI routes

---
+++ data_analyser_layer/routes/restaurant_route.py
@@ -0,0 +1,32 @@
+from sanic import response, Blueprint
+
+data_analyser_bp = Blueprint('data_analyser_bp')
+
+
+@data_analyser_bp.route('/greeting_message', methods=['POST'])
+async def greeting_handler(request):
+    name = request.json.get('name')
+    resp = {
+        "resp_message": "Hello " + name
+    }
+    return response.json(resp)
+
+
+@data_analyser_bp.route('/restaurants', methods=['GET'])
+async def get_restaurants(request):
+    limit = request.args.get('limit')
+    offset = request.args.get('offset')
+    print("printing request to get restaurants :- ", limit, offset)
+    from models.restaurants import Restaurants
+    restaurants = await (Restaurants.query.limit(limit).offset(offset).gino.all())
+    print(restaurants)
+    final_list = []
+    for restaurant in restaurants:
+        final_list.append({'id': restaurant.id, 'name': restaurant.name, 'external_id': restaurant.external_id,
+                           'image_url': restaurant.image_url, 'review_count': restaurant.review_count,
+                           'rating': restaurant.rating, 'address': restaurant.address, 'phone': restaurant.phone,
+                           'categories': restaurant.categories, 'price': restaurant.price})
+
+    return response.json(final_list)
+
+
