a22db03020456195874b7b202303f4f539ca38c6
Vasu Sharma <vash6618@colorado.edu>
2021-04-18 03:12:58 -0600

    hiding secret keys and changing dependencies in requirements.txt

--- data_analyser_layer/config.py
+++ data_analyser_layer/config.py
@@ -13,8 +13,8 @@ class DBConstants:
 class S3Config:
     Bucket_name = str(os.getenv('S3_BUCKET_NAME', 'nopaingain'))
     Bucket_region = 'us-east-2'
-    access_id = str(os.getenv('AWS_ACCESS_KEY_ID', 'AKIAYC6GOQJCG3PDUDMI'))
-    secret_key = str(os.getenv('AWS_SECRET_ACCESS_KEY', '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'))
+    access_id = str(os.getenv('AWS_ACCESS_KEY_ID'))
+    secret_key = str(os.getenv('AWS_SECRET_ACCESS_KEY'))
     analytics = "analytics.pdf"
 
 
